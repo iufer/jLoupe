2a5d620aa5759c1e4b70b5cafa7fd1d5e8b498ce
Chris Iufer <chris@iufer.com>
2010-11-05 17:10:36 -0700

    import v1.3 working

--- jquery.jloupe.js
+++ jquery.jloupe.js
@@ -1,5 +1,5 @@
 /*
- jQuery Loupe v1.2
+ jQuery Loupe v1.3
  Copyright (C) 2010 Chris Iufer (chris@iufer.com)
 
  This program is free software: you can redistribute it and/or modify
@@ -17,44 +17,72 @@
 
 */
 
-$(function(){
-	var jLoupe = {
-		loupe: {'width':200, 'height':200},
-		margin: {x:6, y:6},
-		cursorOffset: {x:10, y:10},
-		bgColor: '#999999'
+jQuery.fn.jloupe = function(o){
+	var version = '1.3';
+	var options = {		
+		width:200,
+		height:200,
+		margin:6,
+		cursorOffsetX:10,
+		cursorOffsetY:10,
+		radiusLT:0,
+		radiusLB:100,
+		radiusRT:100,
+		radiusRB:100,
+		color: '#999999',
+		image: false,
+		repeat: false
 	};
 	
-	jLoupe.radius = jLoupe.loupe.width/2;
-	
-	$('<div id="thejLoupe" />')
+	if(o) jQuery.extend(options, o);
+		
+	var loupe = $('<div />').addClass('thejloupe')
 		.css('position','absolute')
-		.css('width',jLoupe.loupe.width+'px')
-		.css('height',jLoupe.loupe.height+'px')
-		.css('backgroundColor', jLoupe.bgColor)		
+		.css('width',options.width +'px')
+		.css('height',options.height +'px')
 		.hide()
 		.appendTo('body');
+		
+	if(options.color) loupe.css('backgroundColor', options.color)
 	
-	$('<div id="zoomWrapper" />')
-		.css('width',jLoupe.loupe.width-jLoupe.margin.x*2+'px')
-		.css('height',jLoupe.loupe.height-jLoupe.margin.y*2+'px')
+	
+	if(options.repeat) loupe.css('backgroundRepeat', 'repeat');	
+	else loupe.css('backgroundRepeat', 'no-repeat');	
+			
+	var view = $('<div />').addClass('thejloupeview')
+		.css('width',options.width-options.margin*2 +'px')
+		.css('height',options.height-options.margin*2 +'px')
 		.css('backgroundRepeat','no-repeat')
-		.css('marginLeft', jLoupe.margin.x+'px')
-		.css('marginTop', jLoupe.margin.y+'px')
-		.appendTo('#thejLoupe');
+		.css('marginLeft', options.margin +'px')
+		.css('marginTop', options.margin +'px')
+		.appendTo(loupe);
 
 	if($.support.cssProperty('borderRadius')){
-		$('#thejLoupe, #zoomWrapper')
-			.css('border-bottom-left-radius', jLoupe.radius)
-			.css('border-bottom-right-radius', jLoupe.radius)
-			.css('border-top-right-radius', jLoupe.radius)
-			.css('-moz-border-radius-bottomright', jLoupe.radius)
-			.css('-moz-border-radius-bottomleft', jLoupe.radius)
-			.css('-moz-border-radius-topright', jLoupe.radius);
-	}
-
-
-	$('.jLoupe').each(function(){
+		if(options.image) loupe.css('backgroundImage', 'url('+ options.image +')');
+		
+		$(view)
+			.css('border-top-left-radius', options.radiusLT)
+			.css('border-bottom-left-radius', options.radiusLB)
+			.css('border-bottom-right-radius', options.radiusRB)
+			.css('border-top-right-radius', options.radiusRT)
+			.css('-moz-border-radius-topleft', options.radiusLT)
+			.css('-moz-border-radius-bottomright', options.radiusRB)
+			.css('-moz-border-radius-bottomleft', options.radiusLB)
+			.css('-moz-border-radius-topright', options.radiusRT);
+		if(!options.image || options.repeat) {
+			$(loupe)
+				.css('border-top-left-radius', options.radiusLT)
+				.css('border-bottom-left-radius', options.radiusLB)
+				.css('border-bottom-right-radius', options.radiusRB)
+				.css('border-top-right-radius', options.radiusRT)
+				.css('-moz-border-radius-topleft', options.radiusLT)
+				.css('-moz-border-radius-bottomright', options.radiusRB)
+				.css('-moz-border-radius-bottomleft', options.radiusLB)
+				.css('-moz-border-radius-topright', options.radiusRT);
+		}
+	}		
+		
+	$(this).each(function(){
 		var h = $(this).parent('a').attr('href');
 		var s = $(this).attr('src');
 		s = (h) ? h : s;
@@ -64,8 +92,7 @@ $(function(){
 	.bind('mousemove', function(e){ 
 		var o = $(this).offset();
 		var i = $(this).data('zoom');
-		$('#zoomWrapper').css('backgroundImage', 'url('+ $(i).attr('src') +')');
-					
+		var posx = 0, posy = 0;
 		if(e.pageX || e.pageY){
 			posx = e.pageX;
 			posy = e.pageY;
@@ -74,42 +101,36 @@ $(function(){
 			posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 			posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 		}
-		$('#thejLoupe').offset({top:posy+jLoupe.cursorOffset.y, left:posx+jLoupe.cursorOffset.x});
+		$(loupe).offset({top:posy+options.cursorOffsetY, left:posx+options.cursorOffsetX});
 		
-		zlo = (((posx - o.left) / this.width) * $(i).attr('width') *-1) + (jLoupe.loupe.width/2.5);
-		zto = (((posy - o.top) / this.height) * $(i).attr('height') *-1) + (jLoupe.loupe.height/2.5);
+		zlo = (((posx - o.left) / this.width) * $(i).attr('width') *-1) + (options.width/2.5);
+		zto = (((posy - o.top) / this.height) * $(i).attr('height') *-1) + (options.height/2.5);
 
-		$('#zoomWrapper').css('backgroundPosition', zlo+'px ' + zto+'px');
+		$(view).css('backgroundImage', 'url('+ $(i).attr('src') +')').css('backgroundPosition', zlo+'px ' + zto+'px');
 	})
-	.bind('mouseout', function(){
-		$(this).data('zoom').hide();
-		$('#thejLoupe').hide();
+	.bind('mouseleave', function(){
+		$(loupe).stop(true, true).fadeOut(100);
 	})
-	.bind('mouseover', function(){
-		$(this).data('zoom').show();
-		$('#thejLoupe').show();
+	.bind('mouseenter', function(){
+		$(loupe).stop(true, true).fadeIn();
 	});
 	
-});	
+	return this;
+};
+	
 
 $.support.cssProperty = (function() {
   function cssProperty(p, rp) {
-    var b = document.body || document.documentElement,
-    s = b.style;
-
-    // No css support detected
+    var b = document.body || document.documentElement;
+    var s = b.style;
     if(typeof s == 'undefined') { return false; }
-
-    // Tests for standard prop
     if(typeof s[p] == 'string') { return rp ? p : true; }
-
-    // Tests for vendor specific prop
-    v = ['Moz', 'Webkit', 'Khtml', 'O', 'Ms'],
+    var v = ['Moz', 'Webkit', 'Khtml', 'O', 'Ms'];
     p = p.charAt(0).toUpperCase() + p.substr(1);
-    for(var i=0; i<v.length; i++) {
-      if(typeof s[v[i] + p] == 'string') { return rp ? (v[i] + p) : true; }
-    }
+    for(var i=0; i<v.length; i++) {if(typeof s[v[i] + p] == 'string') { return rp ? (v[i] + p) : true; }}
   }
-
   return cssProperty;
 })();
+
+
+$(function(){ $('.jLoupe, .jloupe').jloupe(); });
